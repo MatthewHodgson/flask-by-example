45680086366e5260a6e67b4f8aae4047faf333c4
Matt Hodgson <matthewhodgson@gmail.com>
2016-02-24 11:35:27 -0500

    debug settings

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
@@ -12,4 +14,5 @@ def hello_name(name):
     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
+    print(os.environ['APP_SETTINGS'])
     app.run()
