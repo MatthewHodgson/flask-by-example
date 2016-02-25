94aef856a6bb7ff952b91f3d619c0db2af949d26
Matt Hodgson <matthewhodgson@gmail.com>
2016-02-25 08:10:14 -0500

    initial db migrate

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import Result
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
 
 @app.route('/<name>')
 def hello_name(name):
